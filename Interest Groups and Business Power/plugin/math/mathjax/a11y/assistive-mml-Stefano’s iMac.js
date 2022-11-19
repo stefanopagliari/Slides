bca931445665251092092a834f2cd51210928812
Stefano Pagliari <stefano.pagliari.1@city.ac.uk>
2022-11-19 22:48:12 +0000

    Slides Business Power

---
+++ Interest Groups and Business Power/plugin/math/mathjax/a11y/assistive-mml-Stefano’s iMac.js
@@ -0,0 +1,75 @@
+---
+margin: 0.04
+theme: css/stefano.css
+---
+
+# Political Business Cycles
+
+---
+
+## Nordhaus: “The Political Business Cycle”(1975)
+
+--
+
+### Assumptions of Nordhaus' Political Business Cycle
+
++ **Myopic voters** reward past economic performance, while while not seeing the long-term effects of policy 
++ **Politicians** as self-interested actors seeking re-election
++ **Phillips Curve:** Macro-economic theory that posits a negative relationship between inflation and unemployment
+
+--
+
+![[PhillipsCurve_ExampleGraph.png]]
+--
+### Nordhaus' Political Business Cycle: the Model
++ **Near election time**, governments have incentives to pump up the economy to reduce unemployment
++ **After the election**, politicians shift to contractionary policy to combat inflation
++ **Result:** suboptimal economic performance
+
+--
+### Nordhaus' Political Business Cycle: the Model
+
+ ![[Nordhaus_PBC.png]]
+
+--
+### Nordhaus' Political Business Cycle: the Evidence
+
++ Weak (but not wholly absent) evidence of electoral cycles in macroeconomic outcomes (Alesina et al. 1992; Alesina & Rosenthal 1995). 
+
+---
+
+## Tufte (1978): "The Political Control of the Economy"
+
+![[Tufte_Political_Control_Book.png|300]]
+
+--
+### Tufte (1978): "The Political Control of the Economy" - The Argument
+
++ Government will deliver **timed** and **clearly attributable** benefits to key voters before elections
+	+ **direct transfers** (e.g., social security, veterans’ benefits, or other direct payments),
+	+ **tax cuts** or delayed hikes,
+	+ **spending increases** or delayed cuts,
+	+ public **hiring** or delayed firing.
+
+--
+### Evidence
+
++ More evidence of cycles in direct transfers and other economic policies than in macroeconomic outcomes
++ Incumbents more prone to manipulate the timing of policy implementation than policies themselves.
+
+--
+###  Timing of Elections
+
+- Incumbents likely to schedule elections to coincide with economic expansions
+
+---
+
+## What influences political business cycles?
+
++ **Voters’ rational expectations**
+	+ Voters can foresee the policymakers' incentives
+	+ Smaller and less regular electoral cycles 
++ **External constraints** on policymakers
+	+ Independence of central bank limit the capacity of government to manipulate monetary policy
++ **Number of parties in government**
+	+ When government is divided across different parties, target of economic responsibility is less clear to voters
